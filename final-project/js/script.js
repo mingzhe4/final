ea35251bf79c809310dfdfa93bd86c33e0dda2f2
mingzhe4 <112341928+mingzhe4@users.noreply.github.com>
2022-12-05 14:44:23 -0500

    s

--- final-project/js/script.js
+++ final-project/js/script.js
@@ -1,7 +1,10 @@
 
 //textillate library
 $('.slogan').textillate({
+    //start animation
     in: { effect: 'flash' },
+    //end animation
     out: { effect: 'swing', sync: true },
+    //repeat
     loop: true
 });
